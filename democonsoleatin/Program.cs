0f0ca1dc39429ac79749331c765554245145e9e7
Atin Gupta <atingupta2005@gmail.com>
2021-06-12 02:32:47 +0000

    Add project files.

---
+++ democonsoleatin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using applibraryjulyeyatin;
+
+namespace democonsoleatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //app obj = new app();
+            //obj.Display();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
